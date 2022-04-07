1fa0b9f26808144fd899c461588614f0a0f605c6
Tchewi Atchoum <tchewiatchoum@gmail.com>
2022-04-07 14:52:16 +0200

    add file manuel-encryption

---
+++ files/manual-encryption.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# Author : Quentin Le Ray, Ryan Sauge
+# Date : 07.04.2022
+# Description : Manually encrypt a wep message given the WEP key
+
+
+
